00490b7d55362eeb38a0932c01491f8419b6ecf5
Shreyash <shreyashtekade2503@gmail.com>
2022-08-21 21:51:46 +0530

    first commit

---
+++ src/Catergory.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './categories.styles.scss'
+
+export const Catergory = () => {
+const categories=[
+  {
+    "id": 1,
+    "category": "hats",
+    "src": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "category": "jackets",
+    "src": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "category": "sneakers",
+    "src": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens",
+    "src": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens",
+    "src": "https://i.ibb.co/R70vBrQ/men.png"
+  }
+]
+    return (
+
+        <div className="categories-container">
+            {categories.map((category)=>{
+                return <div className="category-container" key={category.id}>
+                <div className="category-image">
+                    <img src={category.src} alt="" className="background-image" />
+                </div>
+                <div className="category-body-container">
+                    <h2>{category.category}</h2>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+             })}
+        
+        </div>
+    )
+}
